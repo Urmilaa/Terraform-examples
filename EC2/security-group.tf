c843438bcee75c5764bc3b16e4acbccc0e0812cd
Urmilaa <urmikher@gmail.com>
2026-07-13 13:53:09 +0530

    Create security-group.tf

---
+++ EC2/security-group.tf
@@ -0,0 +1,52 @@
+
+  # Default vpc
+
+resource "aws_default_vpc" "default" {
+  tags = {
+    Name = "Default VPC"
+  }
+}
+# Security group
+
+resource "aws_security_group" "my_security_group" {
+
+  name        = "automate_sg"
+  description = "Allow SSH, HTTP and HTTPS"
+
+  vpc_id = aws_default_vpc.default.id
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "mysecurity"
+  }
+}
