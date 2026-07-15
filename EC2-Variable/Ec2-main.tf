5648ecc061088e44a9677397012d08284cc4595a
Urmilaa <urmikher@gmail.com>
2026-07-15 18:09:52 +0530

    Add HTTPS ingress rule to security group
    Added an ingress rule for HTTPS traffic on port 443.

--- EC2-Variable/Ec2-main.tf
+++ EC2-Variable/Ec2-main.tf
@@ -24,6 +24,17 @@ resource "aws_security_group" "web_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+
+    description = "HTTPS"
+
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
 
     from_port   = 0
