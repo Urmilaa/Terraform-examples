a57d920a357fb26035502f04fc2c5a639f3a7986
Urmilaa <urmikher@gmail.com>
2026-07-15 20:09:28 +0530

    Create security group and update EC2 instance settings
    Added a security group for web access and updated EC2 instance configuration.

--- EC2-Count/ec2-count.tf
+++ EC2-Count/ec2-count.tf
@@ -1,9 +1,69 @@
-# Ec2 instance
+resource "aws_security_group" "web_sg" {
 
-      resource "aws_instance" "my_instance"{
+  name        = "terraform-web-sg"
+  description = "Allow SSH and HTTP"
 
-      count = 2
-      key_name = aws_key_pair.my_key.key_name
-      security_groups = [aws_security_group.my_security_group.name]
-      instance_type = var.ec2_instance_type
-      ami = var.ec2_ami_id #ubuntu
+  ingress {
+    description = "SSH"
+
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+
+    description = "HTTP"
+
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+
+    description = "HTTPS"
+
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "terraform-web-sg"
+  }
+}
+# EC2 instance 
+
+resource "aws_instance" "webserver" {
+
+  count = 2
+
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  key_name               = aws_key_pair.my_key.key_name
+ 
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
+
+  user_data = file("userdata.sh")
+
+  tags = {
+    Name = "${var.instance_name}-${count.index + 1}"
+  }
+}
+
