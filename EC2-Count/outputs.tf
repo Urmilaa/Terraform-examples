f687fb152743e28195377c4178afb3edb9c6571e
Urmilaa <urmikher@gmail.com>
2026-07-15 20:12:03 +0530

    Update outputs.tf

--- EC2-Count/outputs.tf
+++ EC2-Count/outputs.tf
@@ -1,17 +1,17 @@
 output "instance_id" {
 
-  value = aws_instance.webserver.id
+  value = aws_instance.webserver[*].id
 
 }
 
 output "public_ip" {
 
-  value = aws_instance.webserver.public_ip
+  value = aws_instance.webserver[*].public_ip
 
 }
 
 output "public_dns" {
 
-  value = aws_instance.webserver.public_dns
+  value = aws_instance.webserver[*].public_dns
 
 }
