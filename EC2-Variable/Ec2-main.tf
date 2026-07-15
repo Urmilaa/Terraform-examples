79b2417a5cab49371e3d34458c7baba50bef5187
Urmilaa <urmikher@gmail.com>
2026-07-15 15:17:03 +0530

    Add EC2 instance and security group configuration

---
+++ EC2-Variable/Ec2-main.tf
@@ -0,0 +1,53 @@
+resource "aws_security_group" "web_sg" {
+
+  name        = "terraform-web-sg"
+  description = "Allow SSH and HTTP"
+
+  ingress {
+    description = "SSH"
+
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+
+    description = "HTTP"
+
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "terraform-web-sg"
+  }
+}
+
+resource "aws_instance" "webserver" {
+
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  key_name               = var.key_name
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
+
+  user_data = file("userdata.sh")
+
+  tags = {
+    Name = var.instance_name
+  }
+}
