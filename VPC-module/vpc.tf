5929dbf5012320467d82de90be2d632d8fd41116
Urmilaa <urmikher@gmail.com>
2026-07-17 21:48:37 +0530

    Create vpc.tf

---
+++ VPC-module/vpc.tf
@@ -0,0 +1,25 @@
+module "vpc" {
+
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "6.0.1"
+
+  name = var.vpc_name
+
+  cidr = var.vpc_cidr
+
+  azs = var.azs
+
+  public_subnets  = var.public_subnets
+  private_subnets = var.private_subnets
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Terraform  = "true"
+    Environment = "Dev"
+  }
+}
