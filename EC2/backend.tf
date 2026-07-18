9d94ac63abc535708dc9d3d05eac3f60e1053c0b
Urmilaa <urmikher@gmail.com>
2026-07-18 17:09:54 +0530

    Update backend.tf

--- EC2/backend.tf
+++ EC2/backend.tf
@@ -2,7 +2,7 @@ terraform {
 
 backend "s3" {
   
-       bucket = "devops-training-terraform-state-bucket1"
+       bucket = "devops-training-terraform-state-bucket12"
        key = "dev/terraform.tfstate"
        region = "us-east-1"
        encrypt = true
