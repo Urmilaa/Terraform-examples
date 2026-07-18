01a13a6d1486ec9974967aa0d6239ae244ac29cc
Urmilaa <urmikher@gmail.com>
2026-07-18 16:53:51 +0530

    Fix syntax for S3 backend configuration

--- EC2/backend.tf
+++ EC2/backend.tf
@@ -1,10 +1,10 @@
 terraform {
 
-backend “s3” {
+backend "s3" {
   
-       bucket = "devops-training-terraform-state-bucket1”
-       key = "dev/terraform.tfstate”
-       region = “us-east-1”
+       bucket = "devops-training-terraform-state-bucket1"
+       key = "dev/terraform.tfstate"
+       region = "us-east-1"
        encrypt = true
        use_lockfile = true  
 
