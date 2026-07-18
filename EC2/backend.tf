f2f1520cfe8393154352740e3e23e922e3a8ee6f
Urmilaa <urmikher@gmail.com>
2026-07-18 16:47:08 +0530

    Change S3 bucket name for Terraform backend

--- EC2/backend.tf
+++ EC2/backend.tf
@@ -2,7 +2,7 @@ terraform {
 
 backend “s3” {
   
-       bucket = "techwithurmila-terraform-state-bucket”
+       bucket = "devops-training-terraform-state-bucket”
        key = "dev/terraform.tfstate”
        region = “us-east-1”
        encrypt = true
